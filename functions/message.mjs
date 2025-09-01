ed232cda2dcea38fad63c9a627a3acf182bc5760
Kevin Hoyt <parkerkrhoyt@gmail.com>
2025-09-01 12:32:56 -0600

    Try formal response object
    In place of custom object.

--- functions/message.mjs
+++ functions/message.mjs
@@ -19,11 +19,9 @@ export default async ( request, context ) => {
   }
 
   if( request.method === 'OPTIONS' ) {
-    return {
-      statusCode: 200,
-      headers,
-      body: 'OK'
-    };
+    return new Response( 'OK', {
+      headers
+    } );
   } 
 
   try {
